8819b0bb9abaae6488ffd0676e73934b94fbab7b
Pratam-Kalligudda <kalliguddapratam642002@gmail.com>
2024-02-26 02:13:25 +0530

    corrected some typo

--- routes/routes.go
+++ routes/routes.go
@@ -9,7 +9,7 @@ func SetRoutes(e *echo.Echo) {
 	e.POST("/api/signup", controllers.Signup)
 	e.POST("/api/login", controllers.Login)
 	// e.GET("/api/levels", controllers.FetchLevelsHandler, controllers.JWTMiddleware())
-	e.GET("/api/guidebook	", controllers.FetchGuidebookHandler)
+	e.GET("/api/guidebook", controllers.FetchGuidebookHandler)
 	e.GET("/api/guidebook/:level", controllers.FetchGuidebookHandler, controllers.JWTMiddleware())
 	e.GET("/api/sublevels/:level", controllers.FetchSublevelsHandler, controllers.JWTMiddleware())
 	e.GET("/api/sublevels", controllers.FetchSublevelsHandler)
